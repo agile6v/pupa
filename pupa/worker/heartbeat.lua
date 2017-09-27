39df42cf182da079eba5b24980bf7bcae7aad18c
agile6v <agile6v@agile6v.com>
2017-09-27 22:45:38 +0800

    improve the heartbeat

--- pupa/worker/heartbeat.lua
+++ pupa/worker/heartbeat.lua
@@ -1,11 +1,27 @@
 
+local http = require "resty.http"
+
 local _M = {}
 
 local function heartbeat(host, i, interval)
     while (true)
     do
-        ngx.sleep(interval)
         ngx.log(ngx.INFO, ">>> thread ", i, ", master : ", host)
+        local httpc = http.new()
+        local res, err = httpc:request_uri(host, {
+            method = "POST",
+            body = "a=1&b=2",
+            headers = {["Content-Type"] = "application/x-www-form-urlencoded"}
+        })
+
+        if not res then
+            ngx.log(ngx.INFO, "failed to request: ", err)
+            return
+        end
+
+        ngx.log(ngx.INFO, "heartbeat response: ", res.status, ", body: ", res.body)
+
+        ngx.sleep(interval)
     end
 end
 
@@ -26,7 +42,7 @@ function _M.start(hosts, interval)
     end
 
     if 0 == ngx.worker.id() then
-        local ok, err = ngx.timer.at(0, handler)
+        local ok, err = ngx.timer.at(5, handler)
         if not ok then
             ngx.log(ngx.ERR, "failed to create the timer: ", err)
             return
