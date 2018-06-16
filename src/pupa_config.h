9d9eca2249013166513d9a4171dfa4f293236744
agile6v <agile6v@agile6v.com>
2018-06-16 22:36:23 +0800

    Add pupa_config.h

---
+++ src/pupa_config.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) agile6v
+ */
+
+#ifndef _PUPA_CONFIG_H
+#define _PUPA_CONFIG_H
+
+#define PUPA_OK         0
+#define PUPA_ERROR     -1
+
+#define pupa_string(str)     { sizeof(str) - 1, (char *) str }
+
+#define pupa_str_set(str, value)                                     \
+    (str)->len = sizeof(value) - 1; (str)->data = (char *) value
+
+typedef struct {
+    int    len;
+    char  *data;
+} pupa_str_t;
+
+#endif //_PUPA_CONFIG_H
